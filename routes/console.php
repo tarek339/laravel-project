0cf80b41f0d2daa37e7beda6c45a299690587ff8
tarek339 <tarekjassine@gmail.com>
2025-08-19 19:52:44 +0200

    refactor: remove Kernel.php and enhance console command scheduling in console.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule commands to run daily at 9:00 AM
+Schedule::command('app:check-drivers-command')
+    ->dailyAt('09:00')
+    ->withoutOverlapping()
+    ->runInBackground();
+
+Schedule::command('app:check-trailers-command')
+    ->dailyAt('09:00')
+    ->withoutOverlapping()
+    ->runInBackground();
+
+Schedule::command('app:check-trucks-command')
+    ->dailyAt('09:00')
+    ->withoutOverlapping()
+    ->runInBackground();
