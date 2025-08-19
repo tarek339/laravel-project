33fc49c19da945a9d7409af95ed960eb66d06bd5
tarek339 <tarekjassine@gmail.com>
2025-08-19 22:22:16 +0200

    refactor: remove timestamps from drivers, trucks, and trailers tables; add timestamps to companies table and new properties to drivers, trailers, and trucks tables

--- database/migrations/2025_08_07_190721_create_trucks_table.php
+++ database/migrations/2025_08_07_190721_create_trucks_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('trucks', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
             $table->string('license_plate')->unique();
             $table->string('identification_number');
--- database/migrations/2025_08_16_105603_add_new_props_to_trailers_table.php
+++ database/migrations/2025_08_16_105603_add_new_props_to_trailers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::table('trailers', function (Blueprint $table) {
             $table->string('assigned_to')->nullable();
             $table->boolean('is_active')->default(false);
+            $table->timestamps();
         });
     }
 
